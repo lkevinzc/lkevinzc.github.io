582d7e6b366dc3c604b7b1d42dfd7d69f6de09c2
Zichen Liu <zichen.liu@shopee.com>
2021-07-12 20:17:53 +0800

    relocate images with thumbs

---
+++ photo/thumbnail.py
@@ -0,0 +1,24 @@
+from PIL import Image
+from glob import glob
+
+MAX_SIDE_LEN = 360
+
+
+def add_margin(pil_img, top, right, bottom, left, color):
+    width, height = pil_img.size
+    new_width = width + right + left
+    new_height = height + top + bottom
+    result = Image.new(pil_img.mode, (new_width, new_height), color)
+    result.paste(pil_img, (left, top))
+    return result
+
+
+ims = glob("./full/*.jpg")
+for imfn in ims:
+    im = Image.open(imfn)
+    w, h = im.size
+    ratio = max(w, h) // MAX_SIDE_LEN
+    thumb_w, thumb_h = w // ratio, h // ratio
+    im.thumbnail((thumb_w, thumb_h))
+    im = add_margin(im, 5, 5, 5, 5, (255, 255, 255))
+    im.save(imfn.replace("full", "thumb"))
